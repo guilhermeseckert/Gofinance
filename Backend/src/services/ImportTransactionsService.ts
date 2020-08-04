4bcd950e36d7d462c7c8ecbf47aec632f565c078
Guilherme Sonnemann Eckert <guilhermeeckert@Guilherme.local>
2020-08-04 10:21:26 -0700

    updated

---
+++ Backend/src/services/ImportTransactionsService.ts
@@ -0,0 +1,85 @@
+import { getCustomRepository, getRepository, In } from 'typeorm';
+import csvParse from 'csv-parse';
+import fs from 'fs';
+import Transaction from '../models/Transaction';
+
+import Category from '../models/Category';
+import TransactionRepository from '../repositories/TransactionsRepository';
+
+interface CSVtransaction {
+  title: string;
+  type: 'income' | 'outcome';
+  value: number;
+  category: string;
+}
+
+class ImportTransactionsService {
+  async execute(filePath: string): Promise<Transaction[]> {
+    const transactionRepository = getCustomRepository(TransactionRepository);
+    const categoriesRepository = getRepository(Category);
+    const contactsReadStream = fs.createReadStream(filePath);
+    const parsers = csvParse({
+      from_line: 2,
+    });
+
+    const parseCSV = contactsReadStream.pipe(parsers);
+    const transactions: CSVtransaction[] = [];
+    const categories: string[] = [];
+
+    parseCSV.on('data', async line => {
+      const [title, type, value, category] = line.map((cell: string) =>
+        cell.trim(),
+      );
+
+      if (!title || !type || !value) return;
+
+      categories.push(category);
+
+      transactions.push({ title, type, value, category });
+    });
+
+    await new Promise(resolve => parseCSV.on('end', resolve));
+
+    const existCategories = await categoriesRepository.find({
+      where: {
+        title: In(categories),
+      },
+    });
+
+    const existCategoriesTitle = existCategories.map(
+      (category: Category) => category.title,
+    );
+
+    const addCategoryTitle = categories
+      .filter(category => !existCategoriesTitle.includes(category))
+      .filter((value, index, self) => self.indexOf(value) === index);
+
+    const newCategories = categoriesRepository.create(
+      addCategoryTitle.map(title => ({
+        title,
+      })),
+    );
+
+    await categoriesRepository.save(newCategories);
+
+    const finalCategories = [...newCategories, ...existCategories];
+
+    const createTransactions = transactionRepository.create(
+      transactions.map(transaction => ({
+        title: transaction.title,
+        type: transaction.type,
+        value: transaction.value,
+        category: finalCategories.find(
+          category => category.title === transaction.category,
+        ),
+      })),
+    );
+
+    await transactionRepository.save(createTransactions);
+    await fs.promises.unlink(filePath);
+
+    return createTransactions;
+  }
+}
+
+export default ImportTransactionsService;
---
+++ readme.md
@@ -0,0 +1,108 @@
+ ** Go Finances **
+
+
+<p align="center">
+  <img width="150" height="100" src="img/logo.svg">
+</p>
+
+<p align="center">
+  <img alt="GitHub language count" src="https://img.shields.io/github/languages/count/commonality/readme-inspector.svg">
+  <img alt="Top language" src="https://img.shields.io/github/languages/top/commonality/readme-inspector.svg">
+</p>
+
+---
+
+<h2 align="center">
+	 🚀 Gofinance is a aplication to help people organizer their fiances and controlle their income and outcome. 🚀
+
+   [Demonstration here]()
+</h2>
+
+
+---
+
+ <p align="center">
+  <img width="2880" height="1640" src="img/web.png">
+</p>
+
+<p align="center">
+  <img width="375" height="667" src="img/mobile.png">
+</p>
+
+<p align="center">
+  <img width="375" height="667" src="img/mobile_1.png">
+</p>
+
+
+</div>
+
+<h4 align="center">
+	🚧  🚀 Under  construction ...  🚧
+</h4>
+
+
+## 🛠 Technology
+
+The following tools were used in the construction of the project:
+
+- [Node.js](https://nodejs.org/en/)
+- [React](https://pt-br.reactjs.org/)
+
+
+## Features
+
+- [x] Register income/outcome
+- [x] Categorized
+- [x] import CSV file
+
+
+
+
+
+### Prerequisites
+
+Before you begin, you will need to have the following tools installed on your computer:
+
+ - [Git](https://git-scm.com)
+ - [Node.js](https://nodejs.org/en/)
+
+In addition, it is good to have an editor to work with the code like
+
+ - [VSCode](https://code.visualstudio.com/)
+
+
+## Project
+
+
+#### Dependencies
+
+- Firebase
+- Sass
+- React
+- Dotenv
+- Redux
+- Styled-components
+
+
+
+### 🎲 Running the the project
+
+``` bash
+# Clone this repository
+$ git clone https://github.com/guilhermeseckert/ClothesEcommerce.git
+
+# Access the project folder in the / cmd terminal
+$ cd CRWN-CLOTHING
+
+# Install the dependencies
+$ yarn
+
+# Run the application in development mode
+$ yarn start
+
+```
+
+
+
+---
+Made with by Guilherme S.Eckert :wave: [Get in touch!](https://www.linkedin.com/in/guilherme-eckert/)
